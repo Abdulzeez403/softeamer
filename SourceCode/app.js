1cb8675bc628d32c007119fa3e7f7baa1fc0fb0c
Abdulazeez403 <Abdulazeezsodiq403@gmail.com>
2022-04-24 14:26:37 -0700

    Mass update

--- SourceCode/app.js
+++ SourceCode/app.js
@@ -1,19 +1,70 @@
-console.log("testing.....");
+
 
 
 //Menu bar toggle
-function menuToggle(){
- let menubar = document.querySelector("#menuIcon");
- let menuItem =document.querySelector("nav");
- menubar.addEventListener("click", showMenuItems);
+/*function menuToggle(){
+ let showbar = document.querySelector("#menuIcon");
+ let hidebar = document.querySelector(".closebtn");
+
+ showbar.addEventListener("click", showMenuItems);
+ hidebar.addEventListener("click", hideMenuItems);
+
  function showMenuItems(e){
-  if(menuItem.style.display === "none"){
-  menuItem.style.display = "block";
-  }else{
-   menuItem.style.display = "none";
-  }
-  
+ let menuItem =document.querySelector(".lists");
+  menuItem.style.width = "100%";
   e.preventDefault();
  }
+
+ function hideMenuItems(e){
+  let menuItem =document.querySelector(".lists");
+   menuItem.style.width = "0";
+   e.preventDefault();
+  }
 };
-menuToggle();
+menuToggle(); */
+
+//Animate on scroll founction
+
+function animateOnScroll(){
+ let targetedDiv= document.querySelector(".scrollMe");
+ const scrollOffset = 100;
+
+
+/* const elementInvView = (el,  offset = 0 )=>{
+   const elementTop =  el.getBoundingClientRect().top;
+   return(
+    elementTop <= ((Window.innerHeight || document.documentElement.clientHeight) - offset)
+   );
+ } */
+ const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+  );
+};
+
+ const displayScrollElement =()=>{
+ targetedDiv.classList.add("scrolled");
+ }
+
+ const hideScrollElement =()=>{
+  targetedDiv.classList.remove("scrolled");
+  }
+
+  const handleScrollAnimation=()=>{
+   if( elementInView (targetedDiv, scrollOffset)){
+    displayScrollElement();
+   }else{
+    hideScrollElement();
+   }
+  }
+
+  window.addEventListener("scroll", ()=>{
+   handleScrollAnimation();
+  })
+ 
+ 
+}
+animateOnScroll();
